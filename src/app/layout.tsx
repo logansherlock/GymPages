7cc5b101d511be789681312318250c0c496d8eeb
MarcosPaAc <isaelpa09@gmail.com>
2025-04-30 15:08:40 -0400

    pulling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css"; // Import Tailwind globally
 import { Navigation } from "./components/navigation";
 import Link from "next/link";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +17,10 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "GymPages",
+  icons: {
+    icon: "/gympages-icons/GymPages_Icon.png", // Update the path to your icon
+    apple: "/gympages-icons/GymPages_Icon.png", // Optional: for Apple devices
+  },
 };
 
 export default function RootLayout({
@@ -29,9 +34,26 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <header className="font-dejaVuMono text-right p-1 flex items-center justify-between border m-2">
-          <Link href="/" className="text-7xl font-semibold align-middle" style={{ WebkitTextStroke: '2px black' }}>
-            GymPages
+          <Link href="/">
+
+          <Image
+          src="/gympages-icons/GymPages_Logo.tiff"
+          alt="GymPages Logo"
+          width={400}
+          height={100}
+          className="rounded-full cursor-pointer"
+          />
+          
           </Link>
+
+          {/* <Link
+            href="/"
+            className="text-7xl font-semibold align-middle"
+            style={{ WebkitTextStroke: '2px black' }}
+          >
+            GymPages
+          </Link> */}
+          
           <Navigation />
         </header>
         <footer></footer>
