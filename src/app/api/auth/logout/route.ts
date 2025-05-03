a6b0625e162f4886c57163a0a8b30d732b4dc799
logansherlock <lks02@proton.me>
2025-05-02 23:03:03 -0400

    p3

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,13 +1,15 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
+  const isProduction = process.env.NODE_ENV === "production";
+
   const response = NextResponse.json({ message: "logged out" });
 
   response.headers.set(
     "Set-Cookie",
     `token=; Path=/; HttpOnly; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT; ${
-      process.env.COOKIE_DOMAIN ? `Domain=${process.env.COOKIE_DOMAIN}; ` : ""
-    }${process.env.NODE_ENV === "production" ? "Secure; SameSite=None;" : ""}`
+      isProduction ? "Secure; SameSite=None; Domain=.gympages.info;" : ""
+    }`
   );
 
   return response;
