e633daf2cc363703655ae63ef13a5d6b087cce8c
logansherlock <lks02@proton.me>
2025-05-05 15:15:04 -0400

    gym search change

--- src/app/gyms/page.tsx
+++ src/app/gyms/page.tsx
@@ -15,7 +15,9 @@ export default function Gym() {
   const { isLoggedIn, username, userID, membership } = useAuth();
   const [gyms, setGyms] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
-  const [equipmentByGym, setEquipmentByGym] = useState<Map<string, string[]>>(new Map());
+  const [equipmentByGym, setEquipmentByGym] = useState<Map<string, string[]>>(
+    new Map()
+  );
   const [selectedGym, setSelectedGym] = useState<any | null>(null);
   const [equipmentSearchTerm, setEquipmentSearchTerm] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
@@ -113,88 +115,20 @@ export default function Gym() {
               className="w-[25%] ml-5 p-2 border border-black rounded text-black m-[1px]"
             />
           </div>
-          <div className="h-[550px] border-[1px] border-black m-2">
-            <GoogleMap
-              zoom={12}
-              center={{ lat: 40.743222191786764, lng: -73.53516023004484 }}
-              mapContainerClassName="map-container"
-              options={{
-                mapTypeControl: false, 
-                clickableIcons: false,
-                // disableDefaultUI: false,
-                streetViewControl: false,
-                fullscreenControl: false
-            }}
-            >
-              {gyms
-                .filter((gym) => {
-                  const nameMatch = gym.gym_name
-                    .toLowerCase()
-                    .includes(searchTerm.toLowerCase());
-                  const equipMatch =
-                    equipmentSearchTerm === "" ||
-                    (Array.isArray(gym.equipment) &&
-                      gym.equipment.some((eq: string) =>
-                        eq
-                          .toLowerCase()
-                          .includes(equipmentSearchTerm.toLowerCase())
-                      ));
-                  return nameMatch && equipMatch;
-                })
-                .map((gym) => (
-                  <MarkerF
-                    key={gym.gym_id}
-                    position={{ lat: gym.location.x, lng: gym.location.y }}
-                    onClick={() => setSelectedGym(gym)}
-                  />
-                ))}
-              {selectedGym && (
-                <InfoWindowF
-                  position={{
-                    lat: selectedGym.location.x,
-                    lng: selectedGym.location.y,
-                  }}
-                  onCloseClick={() => setSelectedGym(null)}
-                >
-                  <div className="flex flex-col justify-center items-center text-black font-bold">
-                    <div className="flex flex-col text-black font-bold text-2xl">
-                      {selectedGym.gym_name}
-                    </div>
-                    {averageRating !== null && (
-                      <div className="flex flex-row items-center text-yellow-600 font-semibold text-lg">
-                        {averageRating.toFixed(1)}⭐️
-                      </div>
-                    )}
-                    <div className="mb-1">{selectedGym.street_address}</div>
-                    <div className="mb-1">
-                      {selectedGym.city}, {selectedGym.state} {selectedGym.zip}
-                    </div>
-                    <div className="mb-1">{selectedGym.phone_number}</div>
-
-                    <div className="flex flex-row justify-center">
-                      <Link
-                        href={`/gyms/${selectedGym.gym_id}`}
-                        className="bg-green-500 font-bold text-white h-6 px-1 py-[3px] text-sm rounded hover:bg-green-600 border-black border-[1px]"
-                      >
-                        View
-                      </Link>
-                    </div>
-                  </div>
-                </InfoWindowF>
-              )}
-            </GoogleMap>
-          </div>
-          <div className="flex flex-col m-2 mt-4 text-white border-black border-[1px] bg-stone-400/75 py-2 px-4">
-            <div
-              className="text-4xl font-bold p-2"
-              style={{
-                WebkitTextStroke: "1px black",
-              }}
-            >
-              SEARCH RESULTS
-            </div>
-            <div>
-              <ul className="px-5 pb-2">
+          <div className="flex flex-row h-[550px] m-2">
+            <div className="w-[60%] border-[1px] border-black">
+              <GoogleMap
+                zoom={11}
+                center={{ lat: 40.743222191786764, lng: -73.53516023004484 }}
+                mapContainerClassName="map-container"
+                options={{
+                  mapTypeControl: false,
+                  clickableIcons: false,
+                  // disableDefaultUI: false,
+                  streetViewControl: false,
+                  fullscreenControl: false,
+                }}
+              >
                 {gyms
                   .filter((gym) => {
                     const nameMatch = gym.gym_name
@@ -204,24 +138,99 @@ export default function Gym() {
                       equipmentSearchTerm === "" ||
                       (Array.isArray(gym.equipment) &&
                         gym.equipment.some((eq: string) =>
-                          eq.toLowerCase().includes(equipmentSearchTerm.toLowerCase())
+                          eq
+                            .toLowerCase()
+                            .includes(equipmentSearchTerm.toLowerCase())
                         ));
                     return nameMatch && equipMatch;
                   })
                   .map((gym) => (
-                    <li key={gym.gym_id}>
-                      <Link
-                        href={`/gyms/${gym.gym_id}`}
-                        className="flex flex-row items-end font-bold block my-1 cursor-pointer hover:scale-[1.25] transition-transform transform origin-left"
-                      >
-                        <span className="text-xl">{gym.gym_name}</span>
-                        <span className="text-sm">
-                          &nbsp;- {gym.city}, {gym.state}
-                        </span>
-                      </Link>
-                    </li>
+                    <MarkerF
+                      key={gym.gym_id}
+                      position={{ lat: gym.location.x, lng: gym.location.y }}
+                      onClick={() => setSelectedGym(gym)}
+                    />
                   ))}
-              </ul>
+                {selectedGym && (
+                  <InfoWindowF
+                    position={{
+                      lat: selectedGym.location.x,
+                      lng: selectedGym.location.y,
+                    }}
+                    onCloseClick={() => setSelectedGym(null)}
+                  >
+                    <div className="flex flex-col justify-center items-center text-black font-bold">
+                      <div className="flex flex-col text-black font-bold text-2xl">
+                        {selectedGym.gym_name}
+                      </div>
+                      {averageRating !== null && (
+                        <div className="flex flex-row items-center text-yellow-600 font-semibold text-lg">
+                          {averageRating.toFixed(1)}⭐️
+                        </div>
+                      )}
+                      <div className="mb-1">{selectedGym.street_address}</div>
+                      <div className="mb-1">
+                        {selectedGym.city}, {selectedGym.state}{" "}
+                        {selectedGym.zip}
+                      </div>
+                      <div className="mb-1">{selectedGym.phone_number}</div>
+
+                      <div className="flex flex-row justify-center">
+                        <Link
+                          href={`/gyms/${selectedGym.gym_id}`}
+                          className="bg-green-500 font-bold text-white h-6 px-1 py-[3px] text-sm rounded hover:bg-green-600 border-black border-[1px]"
+                        >
+                          View
+                        </Link>
+                      </div>
+                    </div>
+                  </InfoWindowF>
+                )}
+              </GoogleMap>
+            </div>
+            <div className="w-[40%] flex flex-col text-white border-black border-[1px] bg-stone-400/75 py-2 px-4 ml-2 overflow-y-auto overflow-x-hidden max-h-[550px]">
+              <div className="flex flex-wrap justify-center">
+                <div
+                  className="text-4xl font-bold p-2"
+                  style={{
+                    WebkitTextStroke: "1px black",
+                  }}
+                >
+                  SEARCH RESULTS
+                </div>
+              </div>
+              <div>
+                <ul className="px-5 pb-2">
+                  {gyms
+                    .filter((gym) => {
+                      const nameMatch = gym.gym_name
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase());
+                      const equipMatch =
+                        equipmentSearchTerm === "" ||
+                        (Array.isArray(gym.equipment) &&
+                          gym.equipment.some((eq: string) =>
+                            eq
+                              .toLowerCase()
+                              .includes(equipmentSearchTerm.toLowerCase())
+                          ));
+                      return nameMatch && equipMatch;
+                    })
+                    .map((gym) => (
+                      <li key={gym.gym_id}>
+                        <Link
+                          href={`/gyms/${gym.gym_id}`}
+                          className="flex flex-row items-end font-bold block my-1 cursor-pointer hover:scale-[1.25] transition-transform transform origin-left"
+                        >
+                          <span className="text-xl">{gym.gym_name}</span>
+                          <span className="text-sm">
+                            &nbsp;- {gym.city}, {gym.state}
+                          </span>
+                        </Link>
+                      </li>
+                    ))}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
