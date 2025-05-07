348f6e647df591177f8a4cddbfe7c618e7a0d249
logansherlock <lks02@proton.me>
2025-05-06 23:21:55 -0400

    package install script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install runtime dependencies
+npm install @react-google-maps/api
+npm install next-auth
+npm install bcrypt
+npm install bcryptjs
+npm install cookie
+npm install react-icons
+npm install pm2
+
+# Install type definitions and dev dependencies
+npm install @types/bcryptjs
+npm install jsonwebtoken
+npm install @types/jsonwebtoken --save-dev
